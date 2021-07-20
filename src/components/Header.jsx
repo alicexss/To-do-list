5c56cee0ddf0a6db30962552b18a1fe04d351bb9
Alice Xavier dos Santos <alicexaviers21@outlook.com>
2021-07-20 19:29:36 -0300

    end of project

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <h1
+      style={{ color: "rgb(247, 41, 113)", fontFamily: "Poppins, sans-serif" }}
+    >
+      Minhas tarefas
+    </h1>
+  );
+};
+
+export default Header;
